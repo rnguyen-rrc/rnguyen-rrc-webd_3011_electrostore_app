af3738cec619385214900af61413912c010f6878
rnguyen-rrc <rnguyen@academic.rrc.ca>
2026-03-27 21:33:24 -0500

    Fix loading error admin/admin_users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,12 @@
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "email", "created_at", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
