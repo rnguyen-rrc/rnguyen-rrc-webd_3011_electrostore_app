563b18372d81b774bd8926a1b9a302c18ba0fa8b
rnguyen-rrc <rnguyen@academic.rrc.ca>
2026-04-01 20:49:46 -0500

    Allow users to create account after guest checkout completes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,21 @@ class UsersController < ApplicationController
 
     user = User.create!(
         username: params[:username],
-        email: order.email,
         password: params[:password],
         password_confirmation: params[:password_confirmation],
-        province_id: order.shipping_province_id
+
+        first_name: order.first_name,
+        last_name: order.last_name,
+
+        email: order.email,
+        phone: order.phone,
+
+        street_name: order.shipping_street,
+        city: order.shipping_city,
+        postal_code: order.shipping_postal_code,
+        province_id: order.shipping_province_id,
+        
+        role: "user"
     )
 
     order.update(user: user)
