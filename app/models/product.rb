5c5a012bc185b304b5c6fd7544d1c8fffa7e0deb
rnguyen-rrc <rnguyen@academic.rrc.ca>
2026-04-02 12:34:32 -0500

    Change label HST to HST/GST on invoice

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock_quantity, numericality: { greater_than_or_equal_to: 0 }
 
   scope :new_products, -> {
